fd03793f1258421b52cd14d78434073a804df775
mostafa ali <elkadydev@gmail.com>
2024-07-07 20:28:46 +0300

    add docs

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-U3ZO22TU.mjs";import"./chunk-STZO6LQF.mjs";import"./chunk-ISRWCDVT.mjs";import"./chunk-YF64RTXV.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
